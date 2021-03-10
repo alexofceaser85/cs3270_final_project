62cc5554fdb26925b2966c387fb3ef58a7b861f4
Alex <contact@alexdecesare.com>
2021-03-10 13:21:19 -0500

    Added docstring for the parse csv method
    Added a docstring for the parse csv method

--- utils.py
+++ utils.py
@@ -11,6 +11,10 @@ __version__ = "10-March-2021"
 
 def parse_csv(file_location):
 
+    """
+    parses a csv into a dictionary and returns that dictionary
+    """
+
     aq1_interactions = {}
 
     with open(file_location, newline='') as csvfile:
@@ -20,3 +24,5 @@ def parse_csv(file_location):
         for row in file_content:
             interaction_file_data = row[0].split(',')
             aq1_interactions[(interaction_file_data[0], interaction_file_data[1])] = interaction_file_data[2]
+
+    return aq1_interactions

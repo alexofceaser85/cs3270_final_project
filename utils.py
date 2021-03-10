9455a704b21ce2877992d8721e772e8e0b597b5b
Alexander Ayers <alexayers2000@gmail.com>
2021-03-10 14:37:15 -0500

    Implemented List Conversion
    Updated CSV File to fit Uniprot Requirements.
    
    Additionally, implemented function to create two lists of the Uniprot IDs and whether they interact.

--- utils.py
+++ utils.py
@@ -12,17 +12,36 @@ __version__ = "10-March-2021"
 def parse_csv(file_location):
 
     """
-    parses a csv into a dictionary and returns that dictionary
+    Parses a csv into a dictionary and returns that dictionary
     """
 
     aq1_interactions = {}
 
     with open(file_location, newline='') as csvfile:
         file_content = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
-        counter = 0
 
         for row in file_content:
             interaction_file_data = row[0].split(',')
-            aq1_interactions[(interaction_file_data[0], interaction_file_data[1])] = interaction_file_data[2]
+            aq1_interactions[(interaction_file_data[0], interaction_file_data[1])] = int(interaction_file_data[2])
 
     return aq1_interactions
+
+def construct_lists_of_features_and_labels(filename):
+    """
+    Gets all features and labels from given csv file and
+    returns them as a tuple of lists.
+    """
+    interactions = parse_csv(filename)
+    proteins = []
+    interacts = []
+
+    for key in interactions:
+        proteins.append(key)
+    for key in interactions:
+        interacts.append(interactions[key])
+
+    return (proteins, interacts)
+
+if __name__ == "__main__":
+    construct_lists_of_features_and_labels("data/humanAQ1.csv")
+    

f73962cc3422e4d2ee0b2bdfc8468da757c3ab31
Alex <contact@alexdecesare.com>
2021-03-10 13:05:16 -0500

    Added data and utils class
    Added the AQ1 data and a data folder to hold any other data. Also added a utils class and a method to parse the AQ1 data.

---
+++ utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import csv
+
+"""
+This is the utils class for the project
+"""
+
+__author__ = "Alex DeCesare"
+__version__ = "10-March-2021"
+
+def parse_csv(file_location):
+
+    aq1_interactions = {}
+
+    with open(file_location, newline='') as csvfile:
+        file_content = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
+        counter = 0
+
+        for row in file_content:
+            interaction_file_data = row[0].split(',')
+            aq1_interactions[(interaction_file_data[0], interaction_file_data[1])] = interaction_file_data[2]

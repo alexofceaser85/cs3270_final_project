0cd522d1f89f652d5c3b07e944f4e61fa0022082
Furichous Jones IV <fjones5@my.westga.edu>
2021-03-11 16:19:21 -0500

    STRING to Uniprot conversion and protein similarity
    Implemented protein similarity calculation of interacting proteins, and conversion from STRING id to uniprot id for easier identification and uniformity.

---
+++ interaction_features.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+'''
+Parses out information from UNIPROT into various formats.clscls
+'''
+from typing import List
+import urllib.parse
+import urllib.request
+import csv
+import uniprot_parser
+import sequence_similarity
+import pprint
+from progress.bar import IncrementalBar
+
+__author__ = "Alexander Ayers"
+__version__ = "2-17-2021"
+
+degree = 33
+sigma = 1.0
+kappa = 0.37
+
+def convert_id_and_calculate_similarity(loaded_filename, saved_filename):
+	url = 'https://www.uniprot.org/uploadlists/'
+
+	full_proteins = []
+	interactions_string = {}
+	with open(loaded_filename, newline="") as interactions_file:
+		interaction_reader = csv.DictReader(interactions_file)
+
+		for interaction in interaction_reader:
+			protein1 = interaction["node1_string_id"]
+			protein2 = interaction["node2_string_id"]
+			interactions_string[protein1, protein2] = interaction["combined_score"]
+			if protein1 not in full_proteins:
+				full_proteins.append(protein1)
+			if protein2 not in full_proteins:
+				full_proteins.append(protein2)
+	
+	protein_line = " ".join(full_proteins)
+	params = {
+		'from': 'STRING_ID',
+		'to': 'ACC',
+		'format': 'txt',
+		'query': protein_line
+	}
+
+	data = urllib.parse.urlencode(params)
+	data = data.encode('utf-8')
+	req = urllib.request.Request(url, data)
+	with urllib.request.urlopen(req) as f:
+		response = f.read()
+
+	interactions_uniprot = {}
+	protein_data, id_conversion = uniprot_parser.parse_txt_file(response.decode("utf-8"))
+
+	total_interactions = 0
+	for protein1, protein2 in interactions_string:
+		if protein1 in id_conversion and protein2 in id_conversion:
+			total_interactions = total_interactions + 1
+
+	bar = IncrementalBar('Processing', max=total_interactions)
+	output_data = []
+	for protein1,protein2 in interactions_string:
+		if protein1 in id_conversion and protein2 in id_conversion:
+			uniprot1 = id_conversion[protein1]
+			uniprot2 = id_conversion[protein2]
+			
+			uniprot1_data = protein_data[uniprot1]
+			uniprot2_data = protein_data[uniprot2]
+
+			uniprot1_sequeunce = uniprot1_data["sequence"]
+			uniprot2_sequeunce = uniprot2_data["sequence"]
+
+			similarity = sequence_similarity.compute_similarity(
+				uniprot1_sequeunce, uniprot2_sequeunce, degree, sigma, kappa)
+
+			interaction_score = interactions_string[protein1, protein2]
+			interaction_features = [interaction_score, similarity]
+
+			interactions_uniprot[uniprot1, uniprot2] = interaction_features
+			interactions_uniprot[uniprot2, uniprot1] = interaction_features
+			output_data.append(
+				{'protein1': uniprot1, 'protein2': uniprot2, 'interaction_score': interaction_score, 'similarity': similarity})
+			bar.next()
+
+	bar.finish()
+	pprint.pprint(interactions_uniprot["P01019", "P29972"])
+
+	with open(saved_filename, mode='w', newline='') as csv_file:
+		fieldnames = ['protein1', 'protein2', 'interaction_score', 'similarity']
+		writer = csv.DictWriter(csv_file, fieldnames=fieldnames )
+
+		writer.writeheader()
+		for data in output_data:
+			writer.writerow(data)
+
+if __name__ == "__main__":
+    convert_id_and_calculate_similarity("interactions.csv", "interaction_data.csv")
---
+++ sequence_similarity.py
@@ -0,0 +1,126 @@
+import math
+
+def compute_similarity(avec, bvec, degree, sigma, kappa):
+	min_length = min(len(avec),len(bvec))
+	avec = avec[:min_length]
+	bvec = bvec[:min_length]
+	
+	half_length = len(avec) // 2
+
+	L = 21
+	'''EXTRACT ACCEPTORS'''
+
+	avec_first_half = avec[0 : half_length]
+	bvec_first_half = bvec[0 : half_length]
+
+	alen = len(avec_first_half)
+	blen = len(bvec_first_half)
+	splice_site = alen // 2
+
+	''' A C C E P T O R     S I M I L A R I T Y '''
+
+	sumk_acc = 0.0
+	sum_acc = 0.0
+
+	for k in range(1, degree+1):
+		for i in range(0, alen - k + 1):
+			l = 1
+			while (l <= (round(kappa * abs(splice_site - l)) + 1)) and (l < L):
+				match = True
+				j = i
+				while (j < i + k) and match:
+					if (j - l < 0):
+						match = False
+						break
+					else:
+						match = (avec_first_half[j] == bvec_first_half[j - l])
+					j += 1
+				if (match):
+					delta = (1 / (2 * (l + 1)))
+					sumk_acc += (delta * (1 / alen))
+				l += 1
+
+			l = 0
+			while (l <= (round(kappa * abs(splice_site - l)) + 1)) and (l < L):
+				match = True
+				j = i
+				while (j < i + k) and match:
+					if (j + l >= alen):
+						match = False
+						break
+					else:
+						
+						match = (avec_first_half[j] == bvec_first_half[j + l])
+					j += 1
+				if (match):
+					delta = (1 / (2 * (l + 1)))
+					sumk_acc += (delta * (1 / alen))
+				l += 1
+			
+		beta = (2 * (degree - k + 1)) / (degree * (degree + 1))
+		sum_acc += sumk_acc * beta
+
+	''' EXTRACT DONORS '''
+	avec_second_half = avec[half_length : len(avec)]
+	bvec_second_half = bvec[half_length : len(bvec)]
+
+	alen = len(avec_second_half)
+	blen = len(bvec_second_half)
+
+	''' D O N O R     S I M I L A R I T Y '''
+	sumk_don = 0.0
+	sum_don = 0.0
+
+	for k in range(1, degree+1):
+		for i in range(0, blen - k + 1):
+			l = 1
+			while (l <= (round(kappa * abs(splice_site - l)) + 1)) and (l < L):
+				match = True
+				j = i
+				while (j < i + k) and match:
+					if (j - l < 0):
+						match = False
+						break
+					else:
+						match = (avec_second_half[j] == bvec_second_half[j - l])
+					j += 1
+				if (match):
+					delta = (1 / (2 * (l + 1)))
+					sumk_don += (delta * (1 / alen))
+				l += 1
+
+			l = 0
+			while (l <= (round(kappa * abs(splice_site - l)) + 1)) and (l < L):
+				match = True
+				j = i
+				while (j < i + k) and match:
+					if (j + l >= alen):
+						match = False
+						break
+					else:
+						match = (avec_second_half[j] == bvec_second_half[j + l])
+					j += 1
+				if (match):
+					delta = (1 / (2 * (l + 1)))
+					sumk_don += (delta * (1 / alen))
+				l += 1
+
+		beta = (2 * (degree - k + 1)) / (degree * (degree + 1))
+		sum_don += sumk_don * beta
+
+	return sum_acc + sum_don
+
+
+def main():
+	avec = "aacccggtttcgagaggtgaagaacatagcagttctttgccaaaatcctccccagaatcatccgtgtcagtcatgtcatcaaacgcgtcgttgtggtccgcatgcaccgaggaagtgaataaaatcggtgtttgtgcgaagcgagagagcagaagtaggatttgcagagttttgatttttcagtttatgttttcttttcagatcttcgtgtttataagatgaagagtttcacaagtaacatggaacagatattgtcaaacgacaaccaactcgctcccacagtcattagaattctgaatagtagaaactcttggtgtctcaactcgtgccatgcttgtttaacatttatcatgggttggttttgcttcctgataaatttattctaatgttttgtaattcagagtaggatttgcagagttttgatttttcagtttatgttttcttttcagatcttcgtgtttataagatgaagagtttcacaagtaacatggaacagatattgtcaaacgacaaccaactcgctcccacagtcattagaattctgaatagtagaaactcttggtgtctcaactcgtgccatgcttgtttaacatttatcatgggttggttttgcttcctgataaatttattctaatgttttgtaattcagaaaatatcacgagcgacaattatgggaagcgatcggcttgcttgaaggctcttgcatcaattaccaacagtttactggacacaatcatcggatttgcatccaccaaaactcggcgaattggagtcgatgtggtagctgaagagcgagctgcaa"
+	bvec = "aaaagagcagctgatcttggagcaaacgtggcattaattgatgcagttgagccaactccacatggacattcatggggaatcggaggaacttgtgcaaatgtcggatgcattcctaaaaagttaatgcaccaagcagcaatcgtcgggaaagaggcaggaataatattataaatatttagagcactaaattcaaaattccagctaaaacacgcagacaaatatggctggaatggcatagatcaagagaaaatcaaacatgattggaatgtgttgtcaaagaatgtgaatgatcgagtaaaagcaaacaattggatttatagagttcaattaaatcagaagtattttttttaattttgtggaaatttttatttttatgaaatttagaaaaatcaattacttcaaatcgtcgggaaagaggcaggaataatattataaatatttagagcactaaattcaaaattccagctaaaacacgcagacaaatatggctggaatggcatagatcaagagaaaatcaaacatgattggaatgtgttgtcaaagaatgtgaatgatcgagtaaaagcaaacaattggatttatagagttcaattaaatcagaagtattttttttaattttgtggaaatttttatttttatgaaatttagaaaaatcaattacttcaatgcctatgccgagtttgtggataaagacaagattgtgataactggtacagacaaaaataaaaccaaggtacgtttggaaaaatgaaaaaagaagttttaaaaaatttgttccgtatacccaaaagttttgcggctt"
+	degree = 33
+	sigma = 1.0
+	kappa = 0.37
+
+	similarity = compute_similarity(avec, bvec, degree, sigma, kappa)
+	print(similarity)
+
+
+if __name__ == "__main__":
+    main()
